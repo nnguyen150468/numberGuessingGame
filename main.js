2eadccf806dbd3b5c01be0c3660a192042680fcc
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-03-05 07:04:00 +0700

    fixed all functions. Added Record button

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ let win = false;
 let history = [];
 let message = '';
 
-let time = 10; // time start from 0
+let record=[];
+
+let gameNumber = 1;
+let timeLimit = 20;
+let time = timeLimit; // time start from 0
 let myTime; // timer will be assign to this variable
 function timeCounting() {
     myTime = setInterval(() => {
@@ -36,37 +40,83 @@ let resetButton = document.getElementById("reset");
 
 let startButton = document.getElementById("startButton");
 
-startButton.addEventListener("click", timeCounting);
+let recordButton = document.getElementById("recordButton");
+
+let timeArea = document.getElementById('timeCount');
+
+let recordArea = document.getElementById("recordArea");
+
+startButton.addEventListener("click", start);
+guessButton.addEventListener("click", guess); //when 'clicked', execute guess()
+resetButton.addEventListener("click", reset);
+recordButton.addEventListener("click",unhideRecord);
 
 function youLose(){
     if((chance==0) || (time==0)){
         resultArea.innerHTML="You lost!";
         timeOut();
-        guessButton.disabled=true;
-
+        //guessButton.disabled=true;
+        recording();
+        showRecord();
     }
 }
 
-guessButton.addEventListener("click", guess); //when 'clicked', execute guess()
+function start(){
+    reset();
+    timeCounting();
+}
 
 function showMessage(){
     resultArea.innerHTML = `${message}`;
     chanceArea.innerHTML=`Chance: ${chance}`;
     historyArea.innerHTML = history;
+    timeArea.innerHTML = time;
 }
-
-resetButton.addEventListener("click", reset);
 
 function reset() {
     chance=chanceLimit;
     history=[];
-    message="";
+    message="Click Start to play the games";
+    time=timeLimit;
     timeOut();
+    win=false;
+    randomNumber=Math.ceil(Math.random()*100);
     showMessage();
  };
 
+function recording(){
+    entry = {
+        history: history,
+        time: time,
+        win: win
+};
+    record.push(entry);
+}
+
+function showRecord(){
+    let para = document.createElement("div");
+    para.innerHTML="<str>Game "+gameNumber+"</str><br>"+
+    "Entries: "+record[gameNumber-1]["history"]+"<br>"+
+    "Time: "+record[gameNumber-1]["time"]+"<br>"+
+    "Win: "+record[gameNumber-1]["win"]+"<br>";
+    document.getElementById("recordArea").appendChild(para);
+    gameNumber++;
+}
+
+function unhideRecord(){
+    let x = document.getElementById("recordArea");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 function guess(){
     userNumber = userGuess.value;
+    if((userNumber)>100 || (userNumber<1)){
+        return resultArea.innerHTML="Error: only a number between 1 and 100 please!";
+    }
     if(history.includes(userNumber)){
         return resultArea.innerHTML="Error: you already chose that number!";
     }
@@ -77,10 +127,13 @@ function guess(){
             message = "yeah correct";
             resultArea.innerHTML = `${message}`;
             win=true;
+            timeOut();
+            recording();
+            showRecord();
         } else if(userNumber > randomNumber){
             chance--;
             message="Too big.... bro";
-            history.push(userNumber);
+            //history.push(userNumber);
             showMessage();
             youLose();
         } else if(userNumber<randomNumber) {
